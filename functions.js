3a06ac5910cb0b7907075051fc6ab97aac5c68fb
Joel <joel.cidmouzinho@gmx.net>
2024-08-29 09:03:46 +0200

    Ab01 Commit Test

---
+++ functions.js
@@ -0,0 +1,6 @@
+let moduloBerechnen = (a, b) => a % b;
+
+console.log(moduloBerechnen(11, 4));  
+console.log(moduloBerechnen(23, 3));  
+console.log(moduloBerechnen(10, 8));
+console.log(moduloBerechnen(9, 4));
---
+++ primitive_datatypes.js
@@ -0,0 +1,5 @@
+const myArray = [42, 'Apfel', true, 1.61, 'Banane'];
+
+for (const element of myArray) {
+    console.log(element);
+}
