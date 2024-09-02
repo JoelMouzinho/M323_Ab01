e6ca292ed115480824bcfda1c951c4fb97b7e9f4
Joel <joel.cidmouzinho@gmx.net>
2024-09-02 14:35:39 +0200

    Ab05

--- functions.js
+++
@@ -1,6 +0,0 @@
-let moduloBerechnen = (a, b) => a % b;
-
-console.log(moduloBerechnen(11, 4));  
-console.log(moduloBerechnen(23, 3));  
-console.log(moduloBerechnen(10, 8));
-console.log(moduloBerechnen(9, 4));
--- primitive_datatypes.js
+++
@@ -1,5 +0,0 @@
-const myArray = [42, 'Apfel', true, 1.61, 'Banane'];
-
-for (const element of myArray) {
-    console.log(element);
-}
